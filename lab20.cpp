d77caca4392623385e77d540ada1ce8f0b695f55
arayapudi626 <rayapudianisha@gmail.com>
2025-10-07 13:59:10 -0700

    update

--- lab20.cpp
+++ lab20.cpp
@@ -1,3 +1,6 @@
+//COMSC 210 | Lab 20 | Anisha Rayapudi
+//IDE used: VS 2022
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -47,22 +50,19 @@ cout << endl << endl;
 int main() {
 cout << fixed << setprecision(2);
 //creating pointer to first chair object
+cout << "First block with default constructor" << endl;
 Chair *chairPtr = new Chair;
 chairPtr->print();
 //creating dynamic chair object with constructor
+cout << "Second block with parameter constructor" << endl;
 double pprices[SIZE] = {100.0, 200.0, 300.0};
 Chair *livingChair = new Chair(3, pprices);
 livingChair->print();
 delete livingChair;
 livingChair = nullptr;
 //creating dynamic array of chair objects
+cout << "Third block with defualt constructor" << endl;
 Chair *collection = new Chair[SIZE];
-collection[0].setLegs(4);
-collection[0].setPrices(441.41, 552.52, 663.63);
-collection[1].setLegs(4);
-collection[1].setPrices(484.84, 959.59, 868.68);
-collection[2].setLegs(4);
-collection[2].setPrices(626.26, 515.15, 757.57);
 for (int i = 0; i < SIZE; i++)
 collection[i].print();
 return 0;
